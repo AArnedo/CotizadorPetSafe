1c321054a2ab296181b9483ff24b7e7d76c98c87
AArnedo <arnedoagustin@gmail.com>
2023-10-24 21:52:43 -0300

    Se agrego script de cotizador, y se modificaron estilos como tambien archivo cotizador.html

---
+++ script/cotizacion.js
@@ -0,0 +1,8 @@
+const cotizarSeguro = () =>{
+    let mascota = document.querySelector('#mascota').value;
+    let edad = document.querySelector('#edad').value;
+    let raza = document.querySelector('#raza').value;
+    let basico = document.querySelector('#basico').value;
+    let intermedio = document.querySelector('#intermedio').value;
+    let premium = document.querySelector('#premium').value;
+}
--- script/movimiento.js
+++ script/movimiento.js
@@ -1,38 +1,37 @@
-const movPage = document.querySelector('.movPage');
-const btn_adelante = document.querySelector('.sigPage');
-const btn_atras = document.querySelector('.volver-pag');
-const btn_final = document.querySelector('.btn-finalizar');
+/* Movimiento de paginas */
+const movPage = document.querySelector(".movPage");
+const btn_adelante = document.querySelector(".sigPage");
+const btn_atras = document.querySelector(".volver-pag");
+const btn_final = document.querySelector(".btn-finalizar");
 
-const progressText = document.querySelectorAll('.paso p');
-const num = document.querySelectorAll('.paso .num');
-const progressCheck = document.querySelectorAll('.paso .check');
+/* Barra de progreso */
+const progressText = document.querySelectorAll(".paso p");
+const num = document.querySelectorAll(".paso .num");
+const progressCheck = document.querySelectorAll(".paso .check");
 
 let max = 2;
 let cont = 1;
 
+btn_adelante.addEventListener("click", function (e) {
+  e.preventDefault();
+  movPage.style.marginLeft = "-25%";
+  num[cont - 1].classList.add("active");
+  progressCheck[cont - 1].classList.add("active");
+  progressText[cont - 1].classList.add("active");
+  cont += 1;
+});
+btn_atras.addEventListener("click", function (e) {
+  e.preventDefault();
+  movPage.style.marginLeft = "0%";
+  num[cont - 2].classList.remove("active");
+  progressCheck[cont - 2].classList.remove("active");
+  progressText[cont - 2].classList.remove("active");
+  cont -= 1;
+});
+btn_final.addEventListener("click", function (e) {
+  e.preventDefault();
+  /* Boton finalizar */
+  Swal.fire("Perfecto!", "Estamos Cotizando");
+});
 
-btn_adelante.addEventListener('click', function(e){
-    e.preventDefault();
-    movPage.style.marginLeft = '-25%';
-    num[cont - 1].classList.add("active");
-    progressCheck[cont - 1].classList.add("active");
-    progressText[cont - 1].classList.add("active");
-    cont += 1;
-})
-btn_atras.addEventListener('click', function(e){
-    e.preventDefault();
-    movPage.style.marginLeft = '0%';
-    num[cont - 2].classList.remove("active");
-    progressCheck[cont - 2].classList.remove("active");
-    progressText[cont - 2].classList.remove("active");
-    cont -= 1;
-})
-btn_final.addEventListener('click', function(e){
-    e.preventDefault();
-    Swal.fire(
-        'Perfecto!',
-        'Estamos Cotizando su Costo',
-      )
-})
 
-/* Alert del botón final */
