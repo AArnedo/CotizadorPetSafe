4ab8c0f7bab564a2040f498528a883cf2c2437d7
AArnedo <arnedoagustin@gmail.com>
2023-10-25 22:03:07 -0300

    Se comenzo a realizar la logica de cotizacion en JS

--- script/cotizacion.js
+++ script/cotizacion.js
@@ -1,8 +1,35 @@
-const cotizarSeguro = () =>{
-    let mascota = document.querySelector('#mascota').value;
-    let edad = document.querySelector('#edad').value;
-    let raza = document.querySelector('#raza').value;
-    let basico = document.querySelector('#basico').value;
-    let intermedio = document.querySelector('#intermedio').value;
-    let premium = document.querySelector('#premium').value;
-}
+const cotizarSeguro = () => {
+  let mascota = document.querySelector("#mascota").value;
+  let edad = document.querySelector("#edad").value;
+  let raza = document.querySelector("#raza").value;
+  let basico = document.querySelector("#basico");
+  let intermedio = document.querySelector("#intermedio");
+  let premium = document.querySelector("#premium");
+
+  let plan = "";
+
+  if (basico.checked) {
+    plan = "basico";
+  } else if (intermedio.checked) {
+    plan = "intermedio";
+  } else if (premium.checked) {
+    plan = "premium";
+
+    if (mascota === "" || edad === "" || raza === "" || plan === "") {
+      mostrarError("#mensaje_error", "FALTA SELECCIONAR ALGUNAS OPCIONES!");
+      return;
+    }
+  }
+
+  divResumen
+
+
+};
+
+const mostrarError = (elemento, mensaje) => {
+  divError = document.querySelector(elemento);
+  divError.innerHTML = `<p class="alerta-error">${mensaje}<p>`;
+  setTimeout(() => {
+    divError.innerHTML = ``;
+  }, 2000);
+};
